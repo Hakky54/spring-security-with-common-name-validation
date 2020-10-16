34272af9f5e2abfea97209b9e88a59ff3d6ae346
Hakky54 <hakangoudberg@hotmail.com>
2020-10-16 12:16:17 +0200

    Simplified authentication

--- src/main/java/nl/altindag/server/config/SecurityConfig.java
+++ src/main/java/nl/altindag/server/config/SecurityConfig.java
@@ -2,42 +2,23 @@ package nl.altindag.server.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.ReactiveAuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
-import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.web.authentication.preauth.x509.SubjectDnX509PrincipalExtractor;
-import org.springframework.security.web.authentication.preauth.x509.X509PrincipalExtractor;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public SecurityWebFilterChain securityWebFilterChain(
-            ServerHttpSecurity serverHttpSecurity,
-            ReactiveAuthenticationManager authenticationManager,
-            X509PrincipalExtractor principalExtractor) {
-        return serverHttpSecurity.authorizeExchange().anyExchange().authenticated()
-                .and()
-                .x509()
-                .principalExtractor(principalExtractor)
-                .authenticationManager(authenticationManager)
-                .and()
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        return http
+                .x509(Customizer.withDefaults())
+                .authorizeExchange(exchanges -> exchanges.anyExchange().authenticated())
                 .build();
-    }
-
-    @Bean
-    public X509PrincipalExtractor principalExtractor() {
-        return new SubjectDnX509PrincipalExtractor();
-    }
-
-    @Bean
-    public ReactiveAuthenticationManager authenticationManager(ReactiveUserDetailsService reactiveUserDetailsService) {
-        return new CertificateCommonNameAuthenticationManager(reactiveUserDetailsService);
     }
 
     @Bean
